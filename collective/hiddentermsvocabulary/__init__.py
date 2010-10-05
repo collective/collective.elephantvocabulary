e195922e056be3d8f501e5fa9890ddcebe198529
Rok Garbas <rok@garbas.si>
2010-10-05 14:59:17 +0200

    need to change to use wrap_vocabulary to return HiddenTermsVocabulary factory

--- collective/hiddentermsvocabulary/__init__.py
+++ collective/hiddentermsvocabulary/__init__.py
@@ -1,2 +1,3 @@
 from collective.hiddentermsvocabulary.vocabulary import HiddenTermsVocabulary
-__all__ = ['HiddenTermsVocabulary',]
+from collective.hiddentermsvocabulary.vocabulary import wrap_vocabulary
+__all__ = ['wrap_vocabulary', 'HiddenTermsVocabulary',]
--- collective/hiddentermsvocabulary/vocabulary.py
+++ collective/hiddentermsvocabulary/vocabulary.py
@@ -1,4 +1,7 @@
+from zope.interface import implements
 from zope.schema.vocabulary import getVocabularyRegistry
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.interfaces import ISource
 
 
 class WrappedVocabulary(object):
@@ -9,14 +12,23 @@ class WrappedVocabulary(object):
     def __getattr__(self, name):
         return getattr(self.vocab, name)
     def __len__(self):
-        return getattr(self.vocab, '__len__')()
+        return len(self.vocab)
+    def search(self, *args, **kw):
+        for term in self.vocab.search(*args, **kw):
+            if term.value not in self.hidden_terms:
+                yield term
     def __iter__(self):
         for term in self.vocab:
             if term.value not in self.hidden_terms:
                 yield term
 
+def wrap_vocabulary(original_vocab, hidden_terms=[],
+                    wrap_with=WrappedVocabulary):
+    return HiddenTermsVocabulary(original_vocab, hidden_terms, wrap_with)
 
 class HiddenTermsVocabulary(object):
+
+    implements(IVocabularyFactory, ISource)
 
     def __init__(self, original_vocab, hidden_terms=[],
                  wrap_with=WrappedVocabulary):
@@ -25,15 +37,19 @@ class HiddenTermsVocabulary(object):
         self.wrap_with = wrap_with
 
     def __call__(self, context):
-        if type(self.original_vocab) in [str, unicode]:
-            import pdb; pdb.set_trace()
-            vocab = getVocabularyRegistry().get(context, self.original_vocab)
+        if isinstance(self.original_vocab, basestring):
+            original_vocab = getVocabularyRegistry().get(
+                    context, self.original_vocab)
         else:
-            vocab = self.original_vocab
+            original_vocab = self.original_vocab
 
         if callable(self.hidden_terms):
-            self.hidden_terms = self.hidden_terms(contex, vocab)
+            self.hidden_terms = self.hidden_terms(contex, original_vocab)
 
-        return self.wrap_with(vocab, self.hidden_terms)
+        if not getattr(original_vocab, 'hidden_terms', None) and \
+           isinstance(original_vocab, list):
+            self.hidden_terms += original_vocab.hidden_terms
+
+        return self.wrap_with(original_vocab, self.hidden_terms)
 
 

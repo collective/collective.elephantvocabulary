6b876cb48728b8a6608e826f1407948ba08b5404
Maurits van Rees <maurits@vanrees.org>
2016-02-09 22:24:56 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -10,41 +10,43 @@ long_description = '\n\n'.join([
         'CHANGES.rst',)
     ])
 
-setup(name='collective.elephantvocabulary',
-      version=version,
-      description="type of zope vocabularies that dont \"forget\", like \
-                   elephants",
-      long_description=long_description,
-      classifiers=[
-          "Programming Language :: Python",
-          "Programming Language :: Zope",
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          ],
-      keywords='zope plone vocabulary',
-      author='Rok Garbas',
-      author_email='rok@garbas.si',
-      url='http://github.com/collective/collective.elephantvocabulary',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'zope.interface',
-          'zope.component',
-          'zope.schema',
-      ],
-      extras_require={
-          'test': [
-              'plone.registry',
-              'zope.dottedname',  # should be dependency of plone.registry
-              'plone.testing [zca]',
-              ]
-      },
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+setup(
+    name='collective.elephantvocabulary',
+    version=version,
+    description="type of zope vocabularies that dont \"forget\", like \
+                 elephants",
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Zope",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+    ],
+    keywords='zope plone vocabulary',
+    author='Rok Garbas',
+    author_email='rok@garbas.si',
+    url='http://github.com/collective/collective.elephantvocabulary',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.component',
+        'zope.schema',
+    ],
+    extras_require={
+        'test': [
+            'plone.registry',
+            'zope.dottedname',  # should be dependency of plone.registry
+            'plone.testing [zca]',
+            ]
+    },
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
